f0db863aef8f3c833c8e9669533055bfb2dabe95
Philip <philippernf@gmail.com>
2018-02-27 16:35:16 +0000

    added backend files for authentication

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
+const auth  = require('../controllers/auth');
+// const secureRoute = require('../lib/secureRoute');
 
-// routes go here
+
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
