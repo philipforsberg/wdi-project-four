2def4a61971263e716846100ee5bdaedcd9aa0af
Philip <philippernf@gmail.com>
2018-03-01 12:02:32 +0000

    rewritten seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -3,31 +3,58 @@ mongoose.Promise = require('bluebird');
 
 const { db, env } = require('../config/environment');
 const Book      = require('../models/book');
+const User = require('../models/user');
 
 
-const bookData = [{
-  title: 'To Kill a Mockingbird',
-  image: 'https://2982-presscdn-29-70-pagely.netdna-ssl.com/wp-content/uploads/2015/07/To-Kill-a-Mockingbird-1st-ed.gif',
-  author: 'Harper Lee',
-  publishedyear: 1960,
-  genre: 'Southern Gothic'
-}, {
-  title: 'Charlie and the Chocolate Factory',
-  image: 'https://cdn.waterstones.com/bookjackets/large/9780/1413/9780141365374.jpg',
-  author: 'Roald Dahl',
-  publishedyear: 1964,
-  genre: 'Childrens fantasy novel'
-}, {
-  title: 'The Hobbit',
-  image: 'http://d1vzko4h6qahek.cloudfront.net/images/2/books/large/HOI2.jpg',
-  author: 'J. R. R. Tolkien',
-  publishedyear: 1937,
-  genre: 'Fantasy'
-}];
+mongoose.connect(db[env]);
 
-mongoose.connect(db[env])
-  .then(() => Book.remove())
-  .then(() => Book.create(bookData))
-  .then(books => console.log(`${books.length} books created!`))
-  .catch(err => console.log(err))
-  .finally(() => mongoose.connection.close());
+User.collection.drop();
+Book.collection.drop();
+
+User
+  .create([{
+    username: 'Sabrina',
+    email: 'sabrina@sabrina.sabrina',
+    password: 'password',
+    passwordConfirmation: 'password'
+  }, {
+    username: 'Robert',
+    email: 'robert@robert.robert',
+    password: 'password',
+    passwordConfirmation: 'password'
+  }, {
+    username: 'James',
+    email: 'james@james.james',
+    password: 'password',
+    passwordConfirmation: 'password'
+  }])
+  .then((users) => {
+    console.log(`${users.length} users created!`);
+
+    return Book.create([{
+      title: 'To Kill a Mockingbird',
+      image: 'https://2982-presscdn-29-70-pagely.netdna-ssl.com/wp-content/uploads/2015/07/To-Kill-a-Mockingbird-1st-ed.gif',
+      author: 'Harper Lee',
+      publishedyear: 1960,
+      genre: 'Southern Gothic'
+    }, {
+      title: 'Charlie and the Chocolate Factory',
+      image: 'https://cdn.waterstones.com/bookjackets/large/9780/1413/9780141365374.jpg',
+      author: 'Roald Dahl',
+      publishedyear: 1964,
+      genre: 'Childrens fantasy novel'
+    }, {
+      title: 'The Hobbit',
+      image: 'http://d1vzko4h6qahek.cloudfront.net/images/2/books/large/HOI2.jpg',
+      author: 'J. R. R. Tolkien',
+      publishedyear: 1937,
+      genre: 'Fantasy'
+    }]);
+  })
+  .then((books) => {
+    console.log(`${books.length} books created!`);
+  })
+  .finally(() => {
+    return mongoose.connection.close();
+  })
+  .catch(err => console.log(err));
